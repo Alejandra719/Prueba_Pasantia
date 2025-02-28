c6346007f785dcba5f0d504e51606c78dd1bb0da
Alejandra719 <alejapiks@gmail.com>
2025-02-28 10:28:28 -0500

    Creacion proyecto

---
+++ src/main/java/com/example/sistemavotacion/sistemavotacion_backend/SistemavotacionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemavotacion.sistemavotacion_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemavotacionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemavotacionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sistemavotacion/sistemavotacion_backend/SistemavotacionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemavotacion.sistemavotacion_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemavotacionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
